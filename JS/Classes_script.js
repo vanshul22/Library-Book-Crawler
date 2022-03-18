d37b617037af9b6c9040154f4d1677c5dcc69e25
Vanshul Kesharwani <vkvanshulkesharwani54@gmail.com>
2022-03-18 12:23:20 +0530

    Initial Commit.

---
+++ JS/Classes_script.js
@@ -0,0 +1,9 @@
+/*
+Website Name - Library-Book-Crawler
+Name : Vanshul Kesharwani
+Date : 18/03/2022
+Version : 1.0.0
+Email : vkvanshulkesharwani54@gmail.com
+Description : This is a Web app for adding different types of books name with their author and description.
+Classes_Script : This JS file take concept of Classes.
+*/
---
+++ README.md
@@ -0,0 +1,4 @@
+# Library-Book-Crawler
+This is a Web application for adding different types of books name with their author and description. Works without refreshing page in javaScript.
+
+To check live page please [Click Here](https://vanshul22.github.io/Library-Book-Crawler/).
